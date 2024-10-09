22396be4d582854c79f7a5d220d4ff7bd0a66d42
Allenzo Paul J R <allenzopaul.jr@gmail.com>
2024-10-09 22:36:07 +0530

    added delete function

--- cmd/todo.go
+++ cmd/todo.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 type Todo struct {
 	Title       string
@@ -9,7 +13,7 @@ type Todo struct {
 	CompletedAt *time.Time //it is a pointed reference because it can be null
 }
 
-//declaring the todos struct of type Todo
+// declaring the todos struct of type Todo
 type Todos []Todo
 
 func (todos *Todos) add(title string) {
@@ -22,4 +26,26 @@ func (todos *Todos) add(title string) {
 
 	//append todo to todos struct
 	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid index")
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = append(t[:index], t[index+1:]...)
+
+	return nil
 }
