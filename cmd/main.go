ebf330e964737da5d4c87688c6945964c30723f5
Allenzo Paul J R <allenzopaul.jr@gmail.com>
2024-10-12 16:36:16 +0530

    added print method

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
 	todos.add("Buy Milk")
 	todos.add("Buy Bread")
-	fmt.Printf("%+v\n\n", todos)
-	todos.delete(0)
-	fmt.Printf("%+v\n\n", todos)
+	todos.toggle(0)
+	todos.print()
 
 }
